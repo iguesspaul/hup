0b6c8e6917ef2807a45ec7d075356d7d2117217d
iguesspaul <paul.l.vantilburg@gmail.com>
2022-12-12 12:50:32 +0100

    tauri init

---
+++ src/lib/views/Header.svelte
@@ -0,0 +1,6 @@
+<header class="flex items-center justify-center w-full h-auto p-3 border-b-2">
+    <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/">Home</a>
+    <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/english">English</a>
+    <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/math">Math</a>
+    <a class="w-auto h-full p-2 mx-1 font-bold bg-teal-100 rounded-lg" href="/science">Science</a>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import "../app.css";
+    import Header from "$lib/views/Header.svelte";
+</script>
+
+
+
+<Header />
+<slot />
